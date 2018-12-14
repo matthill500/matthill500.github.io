6835bb15b7bd2b093c5180c42ace7062742abcbf
matthew hill <matthill500@hotmail.com>
2018-12-14 12:33:59 +0000

    commit4

--- gokard/js/game.js
+++ gokard/js/game.js
@@ -28,7 +28,7 @@ var SceneA = new Phaser.Class({
 			progressBox.fillRect(155, 270, 320, 50);
 			 
 			this.load.audio('pjanoo', [
-			'https://matthill500.github.io/gokard/assets/pjanoo.mp3'
+			'assets/pjanoo.mp3'
 			]);
 			
 			this.load.image('audio', 'assets/audio.png');
@@ -320,7 +320,7 @@ var SceneB = new Phaser.Class({
 		this.load.image('podium', 'assets/podium.png');
 		this.load.image('fireworks', 'assets/fireworks.png');
 		this.load.audio('fireworks', [
-			'../assets/fireworks.mp3'
+			'assets/fireworks.mp3'
 			]);
     },
 
